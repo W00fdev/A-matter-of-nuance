dac66ccb1318edbee9339a665b22f46a85b149c0
Alexander Sudarev <114506972+kiurus7@users.noreply.github.com>
2022-11-06 17:54:58 +0300

    sgdg

--- Assets/CodeBase/Logic/UI/PlayStampSound.cs
+++ Assets/CodeBase/Logic/UI/PlayStampSound.cs
@@ -4,7 +4,10 @@ public class PlayStampSound : MonoBehaviour
 {
     public AudioSource _source;
     public AudioClip clip;
+    public AudioClip clip2;
 
     public void PlayStamp() =>
         _source.PlayOneShot(clip);
+    public void FallStamp() =>
+        _source.PlayOneShot(clip2);
 }
