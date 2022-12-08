e4ad1e54b278588bfb75a6b98f6da1dbba1d7f78
ironslaught <w00f.devs@gmail.com>
2022-12-08 20:04:02 +0300

    Daddy fixes 2. Encounters

--- Assets/CodeBase/Data/ScrollData.cs
+++ Assets/CodeBase/Data/ScrollData.cs
@@ -7,7 +7,8 @@ namespace Data
     [CreateAssetMenu(fileName = "New Scroll", menuName = "Scroll Data", order = 52)]
     public class ScrollData : ScriptableObject
     {
-        public string description;
+        [Multiline] public string description;
+        [Space]
         public Variant[] variants;
     }
 
