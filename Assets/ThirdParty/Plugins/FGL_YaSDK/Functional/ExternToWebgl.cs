5dc61e5fe96055cb5c686ffd7a24d69c77e64a2f
ironslaught <w00f.devs@gmail.com>
2022-12-11 15:33:27 +0300

    Yandex SDK Initial commit

---
+++ Assets/ThirdParty/Plugins/FGL_YaSDK/Functional/ExternToWebgl.cs
@@ -0,0 +1,74 @@
+﻿//----------------------------------------
+//    YaSdk_0.20
+//    disigned by  deviousfox and StasRV
+//    developed by deviousfox
+//    used this asset you automaticle accepted license 
+//    (Plugins/FGL_YaSDK/Other/LICENSE)
+//----------------------------------------
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace FGL_YaSdk
+{
+    public class ExternToWebgl : MonoBehaviour
+    {
+
+        [DllImport("__Internal")]
+        public static extern void ShowRewardedAds(int placementID);
+
+
+        [DllImport("__Internal")]
+        public static extern void ShowFullscreenAds();
+       
+        [DllImport("__Internal")]
+        public static extern string GetString(string key);
+
+        [DllImport("__Internal")]
+        public static extern void SetString(string key, string value);
+    }
+}
+
+namespace FGL_YaSdk.Ads
+{
+    public enum ShowResult { closed, success, exception }
+    public interface IYaAdsListener
+    {
+       void OnYaAdsDidFinish(int placementId, ShowResult showResult);
+    }
+
+    public class YaAds
+    {
+        private static List<IYaAdsListener> adsListeners = new List<IYaAdsListener>();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="listener"></param>
+        public static void AddListener(IYaAdsListener listener)
+        {
+            adsListeners.Add(listener);
+        }
+
+        public static void ShowRewardAds(int placementId)
+        {
+            ExternToWebgl.ShowRewardedAds(placementId);
+        }
+
+        public static void ShowFullScreenAds()
+        {
+
+            ExternToWebgl.ShowFullscreenAds();
+        }
+
+        public static void SendAdsCallback(int placementId, ShowResult showResult)
+        {
+            foreach (var listener in adsListeners)
+            {
+                listener.OnYaAdsDidFinish(placementId, showResult);
+            }
+        }
+    }
+        
+}
+
